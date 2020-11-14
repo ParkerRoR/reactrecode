494d31b79ff1251250bdcb0b36497c98b39e1a25
Marcus Souza <marcusprofmc@outlook.com>
2020-11-14 19:47:09 -0300

    alterei o card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react'
+import Card from './components/Card/Card'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  {nome : 'Alan Santana', idade : '20'},
+  {nome : 'Larissa', idade : '21'},
+  {nome : 'Lucas', idade : '21'},
+  {nome : 'Thamires', idade : '24'},
+  {nome : 'Marcus', idade : '22'},
+  
+]
+
+
+
+class App extends Component{
+
+  mostraNumero = () => {
+    return 'batata'
+  }
+
+  render(){
+    return(
+      <>
+        <h1>Hello World!</h1>
+        {data.map(pessoa => 
+          <Card nome={pessoa.nome} />
+        )}
+
+        {this.mostraNumero()}
+
+        
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
+
